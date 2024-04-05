4fd56ba29252406d16b9914c5d906380f4b59ad1
eho <psj9258@gmail.com>
2024-04-05 13:17:34 +0900

    day10 commit

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,9 +1,38 @@
 import "./App.css";
 
+// function Header() {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   );
+// }
+
+//화살표식 표현(함수식)
+// const Header = () => {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   );
+// };
+import Button from "./components/Button";
+
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+      </Button>
     </>
   );
 }
---
+++ section04/src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ text, color, children }) => {
+  console.log(text, color);
+  return (
+    <button style={{ color: color }}>
+      {text} - {color.toUpperCase()}
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "black",
+};
+export default Button;
